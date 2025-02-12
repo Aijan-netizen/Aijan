4daa74b1b0c7e0ca1f663cd9069e0a1145211896
Aijan-netizen <aijantilekova5@gmail.com>
2025-02-12 16:26:42 +0600

    Delete pl1 aijan laptop.py

--- pl1 aijan laptop.py
+++
@@ -1,9 +0,0 @@
-# laptop
-laptop = {"brand": "dell", "model": "alienware", "year": 2010}
-print("Brand:", laptop["brand"])
-
-laptop["home"] = True
-print("Updated laptop dictionary:", laptop)
-
-laptop["year"] = 2019
-print("Modified laptop dictionary:", laptop)
