6f2e98db6f3ea38f0b811fe04397610d2ce72e50
Aijan-netizen <aijantilekova5@gmail.com>
2025-02-21 17:15:01 +0600

    Update str.py

--- str.py
+++ str.py
@@ -55,21 +55,48 @@ print()
 
 # Методы строк
 
-s = "hello world"
+print("Изменение регистра")
+s = "Hello world"
 print(s.upper()) 
 # HELLO WORLD
+print(s.lower())  
+# "hello world"
+print(s.title())  
+# "Hello World" 
 print(s.replace("world", "Python"))
-# hello Python
+# Hello Python
 print(s.split())
-# ['hello', 'world']
+#['Hello', 'world']
 print()
 
+print("Замена и удаление пробелов")
+s = "  Python  "
+print(s.replace(" ", "_"))  
+# "__Python__"
+print(s.strip())
+# Python - удаляет пробелы с обеих сторон 
+
 s = "I am fine"
-print(s.upper())
-#I AM FINE
 print(s.replace("fine", "good"))
 #I am good
-print(s.replace("am", "am not okay right now"))
-#I am not okay right now fine
-print(s.split())
-#['I', 'am', 'fine']
+print(s.replace("fine", "not okay right now"))
+#I am not okay right now
+
+print("Разделение и объединение")
+print(s.split(","))
+#['I am fine']
+b = ["Your", "room","is","clean"]
+print(" ".join(b))
+#Your room is clean
+
+a = "It's okay to not be okay"
+print(a.find("okay"))
+# 5 - ищет первое вхождение "okay" в строке a - индекс.
+#Если "okay" не найдено, .find() вернёт -1.
+print(a.count('o'))
+#4 - сколько раз встречается "о".
+
+print('Проверка содержимого')
+
+
+
