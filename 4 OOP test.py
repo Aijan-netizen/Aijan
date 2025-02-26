f19a330f17ee6b6c943111ac5e464c9edf9626ac
Aijan-netizen <aijantilekova5@gmail.com>
2025-02-26 13:20:05 +0600

    Update 4 OOP test.py

--- 4 OOP test.py
+++ 4 OOP test.py
@@ -30,6 +30,12 @@ class TestUserService(unittest.TestCase):
         UserService.delete_user(1)
         self.assertIsNone(UserService.find_user(1))
 
+    def test_update_user(self):
+        user = User(1, "Aijan", "Tilekova", datetime(2005, 12, 20))
+        UserService.add_user(user)
+        UserService.update_user(1, name="UpdatedName")
+        self.assertEqual(UserService.find_user(1).name, "UpdatedName")
+
 class TestUserUtil(unittest.TestCase):
     def test_generate_email(self):
         email = UserUtil.generate_email("Aijan", "Tilekova", "example.com")
@@ -40,6 +46,16 @@ class TestUserUtil(unittest.TestCase):
         weak_password = "12345"
         self.assertTrue(UserUtil.is_strong_password(strong_password))
         self.assertFalse(UserUtil.is_strong_password(weak_password))
+
+    def test_generate_user_id(self):
+        user_id = UserUtil.generate_user_id()
+        self.assertTrue(isinstance(user_id, int) and len(str(user_id)) == 9)
+
+    def test_validate_email(self):
+        valid_email = "aijan.tilekova@example.com"
+        invalid_email = "aijan@tilekova@example"
+        self.assertTrue(UserUtil.validate_email(valid_email))
+        self.assertFalse(UserUtil.validate_email(invalid_email))
 
 if __name__ == "__main__":
     unittest.main()
