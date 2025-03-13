d9a4268ad6bbc7f0a8d56a0676554e2070a7643f
Aijan-netizen <aijantilekova5@gmail.com>
2025-03-13 11:31:45 +0300

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,54 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton
+from model import Calculator
+from view import CalculatorWindow
+
+class CalculatorController:
+    def __init__(self):
+        self.calculator = Calculator()  # Создаем объект калькулятора
+        self.window = CalculatorWindow(self.calculator)  # Передаем калькулятор в окно
+        self.setup()
+
+    def setup(self):
+        # Добавляем обработчики событий
+        self.window.button0.clicked.connect(lambda: self.on_button_click('0'))
+        self.window.button1.clicked.connect(lambda: self.on_button_click('1'))
+        self.window.button2.clicked.connect(lambda: self.on_button_click('2'))
+        self.window.button3.clicked.connect(lambda: self.on_button_click('3'))
+        self.window.button4.clicked.connect(lambda: self.on_button_click('4'))
+        self.window.button5.clicked.connect(lambda: self.on_button_click('5'))
+        self.window.button6.clicked.connect(lambda: self.on_button_click('6'))
+        self.window.button7.clicked.connect(lambda: self.on_button_click('7'))
+        self.window.button8.clicked.connect(lambda: self.on_button_click('8'))
+        self.window.button9.clicked.connect(lambda: self.on_button_click('9'))
+        self.window.button_plus.clicked.connect(lambda: self.on_operator_click('+'))
+        self.window.button_minus.clicked.connect(lambda: self.on_operator_click('-'))
+        self.window.button_mul.clicked.connect(lambda: self.on_operator_click('*'))
+        self.window.button_div.clicked.connect(lambda: self.on_operator_click('/'))
+        self.window.button_clear.clicked.connect(self.on_clear)
+        self.window.button_equals.clicked.connect(self.on_equals)
+
+    def on_button_click(self, char):
+        """Добавляем символ в выражение калькулятора"""
+        self.calculator.add_to_expression(char)
+        self.window.input.setText(self.calculator.get_expression())
+
+    def on_operator_click(self, operator):
+        """Добавляем оператор в выражение калькулятора"""
+        self.calculator.add_to_expression(operator)
+        self.window.input.setText(self.calculator.get_expression())
+
+    def on_clear(self):
+        """Очистить выражение калькулятора"""
+        self.calculator.clear_expression()
+        self.window.input.clear()
+
+    def on_equals(self):
+        """Вычислить результат выражения"""
+        result = self.calculator.calculate()
+        self.window.input.setText(str(result))
+
+if __name__ == '__main__':
+    app = QApplication([])
+    controller = CalculatorController()
+    controller.window.show()
+    app.exec_()
