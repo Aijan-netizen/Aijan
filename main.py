2f704326ab021c2da88b35aeb539ba07d228ac5f
Aijan-netizen <aijantilekova5@gmail.com>
2025-04-11 14:19:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ def main():
     dao = EmployeeDAO()
 
     # 1. Insert
-    emp = Employee(name="Айжан", position="Тестировщик", salary=100000, hire_date=str(datetime.now().date()))
+    emp = Employee(name="Aizhan", position="Teacher", salary=100000, hire_date=str(datetime.now().date()))
     dao.insert(emp)
 
     # 2. Get All
-    print("Все сотрудники:")
+    print("All Employees:")
     for e in dao.get_all():
         print(e)
 
     # 3. Get by ID
-    print("\nСотрудник с ID = 1:")
+    print("\nEmployee with ID = 1:")
     print(dao.get_by_id(1))
 
     # 4. Update
     emp_to_update = dao.get_by_id(1)
     if emp_to_update:
         emp_to_update.salary = 120000
-        emp_to_update.name = "Айжан Обновлённая"
+        emp_to_update.name = "Aizhan Updated"
         dao.update(emp_to_update)
 
     # 5. Delete
     dao.delete(1)
-    print("\nПосле удаления:")
+    print("\nAfter Deletion:")
     for e in dao.get_all():
         print(e)
 
